ef071ddd0ee885e50dec6b3e6346ab0b76d829b0
bao141004 <vuvanbao347@gmail.com>
2023-11-13 19:13:54 +0700

    update catgiaodien map contact

--- view/lienhe.php
+++ view/lienhe.php
@@ -58,6 +58,7 @@
 
           <div class="col-md-6 d-flex">
           	<div id="map" class="bg-white"></div>
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3723.8680883985685!2d105.7443465106364!3d21.03796348053281!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x313455305afd834b%3A0x17268e09af37081e!2sT%C3%B2a%20nh%C3%A0%20FPT%20Polytechnic.!5e0!3m2!1sen!2s!4v1699851442456!5m2!1sen!2s" width="100000" height="550" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
           </div>
         </div>
       </div>
