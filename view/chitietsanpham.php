717afc144b9a3a05d68834f00c36e76525d4c858
quytoon <quyton69@gmail.com>
2023-11-14 15:17:55 +0700

    update catgiaodien chitietsanpham

--- view/chitietsanpham.php
+++ view/chitietsanpham.php
@@ -22,7 +22,9 @@
                 ?>
             </div>
             <div class="col-lg-6 product-details pl-md-5 ftco-animate">
-                <h3><?=$TenSanPham?></h3>
+                <h3>
+                    <?= $TenSanPham ?>
+                </h3>
                 <div class="rating d-flex">
                     <p class="text-left mr-4">
                         <a href="#" class="mr-2">5.0</a>
@@ -39,16 +41,20 @@
                         <a href="#" class="mr-2" style="color: #000;">500 <span style="color: #bbb;">Sold</span></a>
                     </p>
                 </div>
-                <p class="price"><span><?=$Gia?> vnđ</span></p>
-                <p><?=$MoTa?>
+                <p class="price"><span>
+                        <?= $Gia ?> vnđ
+                    </span></p>
+                <p>
+                    <?= $MoTa ?>
                 </p>
                 <div class="row mt-4">
                     <div class="col-md-6">
                         <h5>Màu</h5>
                         <div class="row">
-                            <div class="col-2">
+                            <!-- <div class="col-2">
                                 <button class="color-btn" style="background-color: #FF0000"></button>
-                            </div>
+                            </div> -->
+
                         </div>
                     </div>
                 </div>
