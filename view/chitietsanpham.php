9d7a88fd7a5ebab3d593e09c898985b98f9a3cd4
quytoon <quyton69@gmail.com>
2023-11-21 18:25:03 +0700

    updatelink

--- view/chitietsanpham.php
+++ view/chitietsanpham.php
@@ -222,13 +222,13 @@
                 $img = $img_path . $key['img'];
                 echo '<div class="col-md-6 col-lg-3 ftco-animate">
                 <div class="product">
-                    <a href="#" class="img-prod"><img class="img-fluid" src="'.$img.'"
+                    <a href="index.php?act=chitietsanpham&idsp='.$key['IdSanPham'].'" class="img-prod"><img class="img-fluid" src="'.$img.'"
                             alt="Colorlib Template">
                         <span class="status">30%</span>
                         <div class="overlay"></div>
                     </a>
                     <div class="text py-3 pb-4 px-3 text-center">
-                        <h3><a href="#">'.$key['TenSanPham'].'</a></h3>
+                        <h3><a href="index.php?act=chitietsanpham&idsp='.$key['IdSanPham'].'">'.$key['TenSanPham'].'</a></h3>
                         <div class="d-flex">
                             <div class="pricing">
                                 <p class="price"><span class="mr-2 price-dc">'.$Gia.'</span><span
@@ -241,7 +241,7 @@
                                     class="add-to-cart d-flex justify-content-center align-items-center text-center">
                                     <span><i class="ion-ios-menu"></i></span>
                                 </a>
-                                <a href="#" class="buy-now d-flex justify-content-center align-items-center mx-1">
+                                <a href="index.php?act=themgiohang&idsp='.$key['IdSanPham'].'" class="buy-now d-flex justify-content-center align-items-center mx-1">
                                     <span><i class="ion-ios-cart"></i></span>
                                 </a>
                                 <a href="#" class="heart d-flex justify-content-center align-items-center ">
