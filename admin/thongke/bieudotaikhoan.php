bb7c50402c5ef96ff0c1c428207a0a88d2fb481e
bao141004 <vuvanbao347@gmail.com>
2023-11-23 21:09:07 +0700

    update thong ke bieu do dang lam do chua xong

--- admin/thongke/bieudotaikhoan.php
+++ admin/thongke/bieudotaikhoan.php
@@ -35,7 +35,7 @@
     const data = google.visualization.arrayToDataTable([
       ['Tài khoản', 'Số lượng'],
       <?php
-      foreach ($thongketk as $key) {
+      foreach ($listthongketaikhoan as $key) {
         extract($key);
         echo "['Só tài khoản khách hàng có trong website là : $soluong', '$soluong']";
       }
@@ -56,3 +56,34 @@
 
 
 </script>
+<html>
+  <head>
+    <script type="text/javascript" src="https://www.gstatic.com/charts/loader.js"></script>
+    <script type="text/javascript">
+      google.charts.load('current', {'packages':['corechart']});
+      google.charts.setOnLoadCallback(drawChart);
+
+      function drawChart() {
+        var data = google.visualization.arrayToDataTable([
+          ['Year', 'Sales', 'Expenses'],
+          ['2013',  1000,      400],
+          ['2014',  1170,      460],
+          ['2015',  660,       1120],
+          ['2016',  1030,      540]
+        ]);
+
+        var options = {
+          title: 'Thống kê tài khoản',
+          hAxis: {title: 'Year',  titleTextStyle: {color: '#333'}},
+          vAxis: {minValue: 0}
+        };
+
+        var chart = new google.visualization.AreaChart(document.getElementById('chart_div'));
+        chart.draw(data, options);
+      }
+    </script>
+  </head>
+  <body>
+    <div id="chart_div" style="width: 1200px; height: 500px;"></div>
+  </body>
+</html>
